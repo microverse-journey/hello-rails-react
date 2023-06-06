ae391a554053f797fcfb98e406d5ce1f5bcf9df4
mrprotocoll <lekanvgbg@gmail.com>
2023-06-06 16:55:03 +0100

    Add app component with react router

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+function App(){
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </Router>
+    </>
+}
+
+export default App;
