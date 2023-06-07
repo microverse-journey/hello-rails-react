d2b15e5eaf61cb64045de37fbe22395092f34310
mrprotocoll <lekanvgbg@gmail.com>
2023-06-07 06:45:19 +0100

    Fix linters errors

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ import App from './components/App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getGreetings } from '../redux/greetingSlice';
 
-let render = true;
 function Greeting() {
-    const { greeting } = useSelector((store) => store.greeting);
-    const dispatch = useDispatch();
+  const { greeting } = useSelector((store) => store.greeting);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (!render) {
-          return;
-        }
-        render = false;
-        dispatch(getGreetings());
-    }, []);
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, []);
   return (
     <h1>{greeting}</h1>
   );
